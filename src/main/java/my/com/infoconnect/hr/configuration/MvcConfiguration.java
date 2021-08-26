17dfb588e1d4d8bb253cdbcc06856a5b047ca4ef
Wilson <wilson.lai@infoconnect.com.my>
2021-08-26 10:31:01 +0800

    - resolve configuration issue

--- src/main/java/my/com/infoconnect/hr/configuration/MvcConfiguration.java
+++ src/main/java/my/com/infoconnect/hr/configuration/MvcConfiguration.java
@@ -9,23 +9,23 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-@Configuration
-@EnableWebMvc
-public class MvcConfiguration implements WebMvcConfigurer {
+//@Configuration
+//@EnableWebMvc
+//public class MvcConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/resources/**")
+//                .addResourceLocations("/resources/");
+//    }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
-        registry.viewResolver(resolver);
-    }
-}
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix("/WEB-INF/jsp/");
+//        resolver.setSuffix(".jsp");
+//        resolver.setViewClass(JstlView.class);
+//        registry.viewResolver(resolver);
+//    }
+//}
